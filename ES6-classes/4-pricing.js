d90124959d37b561ffcd1eabae241ba842183081
bo1atito <shorungbebolatito@gmail.com>
2025-02-01 02:23:35 +0100

    ES6 classes

---
+++ ES6-classes/4-pricing.js
@@ -0,0 +1,52 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be an instance of the Currency Class');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('currency must be an instance of the Currency Class');
+    }
+    this._currency = newCurrency;
+  }
+
+  // method
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  // static method
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion Rate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
---
+++ ES6-classes/5-building.js
@@ -0,0 +1,26 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = sqft;
+    this.evacuationWarningMessage();
+  }
+
+  evacuationWarningMessage() {
+    if (this.constructor.name !== 'Building') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(newSqft) {
+    if (typeof newSqft !== 'number') {
+      throw new TypeError('Sqft must be a number');
+    }
+    this._sqft = newSqft;
+  }
+}
